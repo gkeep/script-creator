49aed9220d274df39341ffad7ceea76b63f421e4
Gennady Koshkin <gkoshkin@hr-link.ru>
2023-10-20 13:03:49 +0300

    s;jrstnijftijgsrij

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import sys
+
+from ui_helper import UIMain
+from PySide2.QtWidgets import QMainWindow, QApplication
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    content = UIMain(MainWindow)
+
+    MainWindow.show()
+    sys.exit(app.exec_())
